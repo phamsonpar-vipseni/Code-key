21ea1492fa82d38df82a4339a3ac38460ef200ef
phamsonpar-vipseni <phamson201229@gmail.com>
2026-07-30 09:36:52 +0700

    Add files via upload

---
+++ Key.js
@@ -0,0 +1,14 @@
+const VALID_KEYS = [
+  {
+    key: "phamson-ios-47CNAO-X37DBF",
+    label: "ĐỊT MẸ",
+    expiresAt: "2026-7-11",
+    maxDevices: 1
+  },
+  {
+    key: "phamson-ios-A1B2C3D4-E5F6G7H8",
+    label: "VIP PRO",
+    expiresAt: "2026-12-31",
+    maxDevices: 5
+  },
+];
